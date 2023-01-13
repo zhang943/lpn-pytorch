4c43e03eb31286ec7790a8a29b5082a5c1872424
Dominic Wong <dominic@mantisropewaytechnologies.com>
2023-01-13 13:51:54 +0000

    Readded inference and transforms methods

--- lpn_pytorch/config/default.py
+++ lpn_pytorch/config/default.py
@@ -25,4 +25,6 @@ _C.MODEL.TARGET_TYPE = 'gaussian'
 _C.MODEL.IMAGE_SIZE = [256, 256]  # width * height, ex: 192 * 256
 _C.MODEL.HEATMAP_SIZE = [64, 64]  # width * height, ex: 24 * 32
 _C.MODEL.SIGMA = 2
+_C.MODEL.TEST = CN()
+_C.MODEL.TEST.POST_PROCESS = True
 _C.MODEL.EXTRA = CN(new_allowed=True)

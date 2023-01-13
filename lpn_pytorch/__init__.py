dcde2ae900ffee32b47a4168d8e53aeac8509061
Dominic Wong <dominic@mantisropewaytechnologies.com>
2023-01-13 12:27:11 +0000

    lpn_pytorch is a python package

---
+++ lpn_pytorch/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0'
+
+import lpn_pytorch.models
+import lpn_pytorch.config
---
+++ lpn_pytorch/models/__init__.py
@@ -0,0 +1,3 @@
+from .lpn import LPN
+from .pose_hrnet import PoseHighResolutionNet
+from .pose_resnet import PoseResNet
